5522dc50d3f2a20ae4c217fdb4f4809249f2c87b
leah_hirschfeld <leah.hirschfeld@gmail.com>
2021-06-24 09:47:51 -0700

    commit tree 5566e5df4e44fc6839b23018cd6cd4b497dffba3

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(userName, userEmail) {
+
+const formData = {
+    name: userName,
+    email: userEmail
+  };
+  
+  const configUser = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+  
+  return fetch("http://localhost:3000/users", configUser)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    const id = object.id;
+    document.body.innerHTML += id
+  })
+  .catch(function(error) {
+    document.body.innerHTML += error.message;
+  })
+}
